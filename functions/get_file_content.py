c7bd08758da7a0ea8bb4b1b108858989a161812d
David Shaw <dshaw11@utk.edu>
2025-12-18 14:54:30 -0500

    working on parsing lorem.txt

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -6,17 +6,19 @@ def get_file_content(working_directory, file_path):
 
     working_dir_abs = os.path.abspath(working_directory)
 
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+    target_path = os.path.normpath(os.path.join(working_dir_abs, file_path))
 
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_path]) == working_dir_abs
 
     if valid_target_dir == False:
         return f'Error: Cannot read "{file_path} as it is outside the permitted working directory'
-    if os.path.isfile(target_dir) == False:
+    
+    print(target_path)
+    if os.path.isfile(target_path) == False:
         return f'Error: File not found or is not a regular file: "{file_path}"'
     
     try:
-        with open(target_dir, "r") as f:
+        with open(target_path, "r") as f:
             content = f.read(MAX_CHARS)
             # After reading the first MAX_CHARS...
             if f.read(1):
