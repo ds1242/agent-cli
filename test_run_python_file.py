459564b47bdcecd1a4d8cab709422d0ef2c47a6c
David Shaw <dshaw11@utk.edu>
2025-12-19 11:10:47 -0500

    initial setup of test file

---
+++ test_run_python_file.py
@@ -0,0 +1,20 @@
+
+
+from functions.run_python_file import run_python_file
+
+
+test():
+    print(run_python_file("calculator", "main.py"))
+
+    print(run_python_file("calculator", "main.py", ["3 + 5"]))
+
+    print(run_python_file("calculator", "tests.py"))
+
+    print(run_python_file("calculator", "../main.py"))
+
+    print(run_python_file("calcualtor", "nonexistent.py"))
+
+    print(run_python_file("calculator", "lorem.txt"))
+
+
+
